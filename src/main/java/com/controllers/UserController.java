c91b1e97473cb786583e824a5d4b2dd7455c8b4a
cnerd <mahadidroid@gmail.com>
2022-08-17 02:05:49 +0600

    RegIssues[Checking Data Transfer Individual] 1.0

--- src/main/java/com/controllers/UserController.java
+++ src/main/java/com/controllers/UserController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
