6cb42bde7fd16b3e552586f963767ed41a9358a8
cnerd <mahadidroid@gmail.com>
2022-08-18 12:50:43 +0600

    Post Work Going [Req]

--- src/main/java/com/controllers/VisitorController.java
+++ src/main/java/com/controllers/VisitorController.java
@@ -1,15 +1,28 @@
 package com.controllers;
 
+import com.models.Post;
+import com.services.PostService;
+import com.services.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/visitor")
 public class VisitorController {
 
-    private final VisitorService visitorService;
+    private final UserService userService;
+    private final PostService postService;
+    public VisitorController(UserService userService, PostService postService) {
 
-    public VisitorController(VisitorService visitorService) {
-        this.visitorService = visitorService;
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    @RequestMapping(value = "/dash", method = RequestMethod.GET)
+    public List<Post> showAllPost() {
+        return postService.getAll();
     }
 
 
