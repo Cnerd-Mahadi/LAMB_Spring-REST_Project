4254e1470598604184fe13fb73e8543661b2ca16
cnerd <mahadidroid@gmail.com>
2022-08-18 16:57:07 +0600

    Post Work Going

--- src/main/java/com/controllers/PostController.java
+++ src/main/java/com/controllers/PostController.java
@@ -29,7 +29,6 @@ public class PostController {
     @RequestMapping(value = "/create-post", method = RequestMethod.POST, consumes = {"application/json"})
     public ResponseEntity<Post> makePost(@RequestBody Post post) {
 
-        post.setPostUserInfo(userService.get(post.getPostUserInfo().getUserId()));
         postService.save(post);
         return ResponseEntity.ok(post);
     }
