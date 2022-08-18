783a0c3d4cd48c3608c4a9852e4357c7c6d3f109
cnerd <mahadidroid@gmail.com>
2022-08-18 16:31:53 +0600

    Post Work Going

--- src/main/java/com/controllers/PostController.java
+++ src/main/java/com/controllers/PostController.java
@@ -5,6 +5,7 @@ import com.models.Post;
 import com.models.User;
 import com.services.DonorService;
 import com.services.PostService;
+import com.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +19,17 @@ import java.util.Map;
 public class PostController {
 
     private final PostService postService;
+    private final UserService userService;
 
-    public PostController(PostService postService) {
+    public PostController(PostService postService, UserService userService) {
         this.postService = postService;
+        this.userService = userService;
     }
 
     @RequestMapping(value = "/create-post", method = RequestMethod.POST, consumes = {"application/json"})
     public ResponseEntity<Post> makePost(@RequestBody Post post) {
 
+        //post.setPostUserInfo(u);
         postService.save(post);
         return ResponseEntity.ok(post);
     }
