444ebcc59d7591d4bfb07dc4d7172ae2ba564350
cnerd <mahadidroid@gmail.com>
2022-08-18 15:42:53 +0600

    Post Work Going [Req1]

---
+++ src/main/java/com/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.controllers;
+
+import com.models.Donor;
+import com.models.Post;
+import com.models.User;
+import com.services.DonorService;
+import com.services.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST, consumes = {"application/json"})
+    public ResponseEntity<Post> makePost(@RequestBody Post post) {
+
+        postService.save(post);
+        return ResponseEntity.ok(post);
+    }
+
+
+
+}
