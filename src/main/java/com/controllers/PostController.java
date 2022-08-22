980e3f1b40e04bea632a3fdd3d0d5da25bc99772
Cnerd Mahadi <mahadidroid@gmail.com>
2022-08-22 07:21:27 +0600

    DEPLOY

--- src/main/java/com/controllers/PostController.java
+++ src/main/java/com/controllers/PostController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/backend")
 public class PostController {
 
