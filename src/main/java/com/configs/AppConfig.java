93ea573947187a8ce8fc80d701071ec715bc42a4
Cnerd Mahadi <mahadidroid@gmail.com>
2022-08-22 07:18:47 +0600

    DEPLOY

--- src/main/java/com/configs/AppConfig.java
+++ src/main/java/com/configs/AppConfig.java
@@ -43,16 +43,16 @@ public class AppConfig implements WebMvcConfigurer {
 
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+//            }
+//        };
+//    }
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
--- src/main/java/com/controllers/PostController.java
+++ src/main/java/com/controllers/PostController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "localhost:3000")
 @RequestMapping("/backend")
 public class PostController {
 
